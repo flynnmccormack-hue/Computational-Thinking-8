fbada5d0000eb5aaf99aab63c2dbd51c127626a6
flynnmccormack-hue <flynnmccormack@seattleacademy.org>
2025-10-22 16:11:20 +0000

    Commit

---
+++ Projects/code_art_intro.py
@@ -0,0 +1,50 @@
+# ###############################################
+# ### SETUP ###
+import turtle
+# ###############################################
+
+t = turtle.Turtle()
+t.speed(10)
+t.penup()
+t.goto(-250, 10)#perfect
+t.color("blue")
+t.pendown()
+
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+
+t = turtle.Turtle()
+t.penup()
+t.goto(100,10)
+t.color("Silver")
+t.pendown()
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-180,-140)
+t.color("Silver")
+t.pendown()
+t.left(350)
+t.forward(175)
+t.left(15)
+t.forward(175)
+# ###############################################
+# ### ENDING ###
+turtle.exitonclick()
+# ###############################################
