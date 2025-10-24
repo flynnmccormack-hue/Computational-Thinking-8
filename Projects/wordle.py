6647a02a6ac05dff0db42e9a9329bf2d6eb88f0d
flynnmccormack-hue <flynnmccormack@seattleacademy.org>
2025-10-24 17:45:34 +0000

    Done

---
+++ Projects/wordle.py
@@ -0,0 +1,60 @@
+import random
+
+# Pick a word at random
+word_list = ["loopy","heart","audio","laugh","trial", "sonic", "waves", "arose", "crane", "irate", "slate", "round", 
+            "growl", "greet", "great", "pause", "sleep", "flynn", "toby", "deco", "mason"]
+hidden_word = random.choice(word_list) 
+print("WORDLE:")
+
+# Repeat for 6 guesses
+for i in range(6):
+    # Guess a word
+    guess_word = input()
+    output = "" 
+    if len(guess_word) is not 5:
+        print("please type only 5 letter words")
+    if len(guess_word) not in word_list:
+        print("please type only words in the list above")
+    # First letter (in python, counting starts at 0 not 1)
+    if guess_word[0] == hidden_word[0]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    if guess_word[1] == hidden_word[1]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    if guess_word[2] == hidden_word[2]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    if guess_word[3] == hidden_word[3]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    if guess_word[4] == hidden_word[4]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+    
+    # Result
+    print(output)
+    if output == "🟩🟩🟩🟩🟩":
+        print("You win")
+        break
+
+print(f"You used {i+1} guesses")
